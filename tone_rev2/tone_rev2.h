3528edc6f735f833c14bac612711948d20debbe0
ぺらねこ <duelist.info@gmail.com>
2021-07-18 20:59:14 +0900

    Add files via upload

--- tone_rev2/tone_rev2.h
+++ tone_rev2/tone_rev2.h
@@ -13,21 +13,23 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( 
-        C(S(KC_E)), S(KC_TAB),  KC_TAB,      KC_0,
-        KC_LSFT,    C(KC_LEFT), C(KC_RIGHT), C(KC_Z)
-    )
-};
+#pragma once
 
-/* Rotary encoder settings */
-bool encoder_update_user(uint8_t index, bool clockwise) {
-   if (clockwise) {
-        tap_code(KC_UP);    //Rotary encoder clockwise
-    } else {
-        tap_code(KC_DOWN);  //Rotary encoder Reverse clockwise
-    }
-    return true;
+#include "quantum.h"
+
+/* This is a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT( \
+    K00, K01, K02, K03, \
+    K04, K05, K06, K07  \
+) \
+{ \
+    { K00, K01, K02, K03, K04, K05, K06, K07 }, \
 }
